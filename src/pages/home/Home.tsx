0724167510df6223ab781fe06928d5d30e043c91
Pedro Raposo <peraposo@hotmail.com>
2023-08-07 12:36:52 +0100

    Fix: Capitalized letter to fix issue

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import { Section } from "~/components/Section";
+
+export const HomeSection = () => {
+  return (
+    <Section
+      sectionProps={{
+        id: "homeSection",
+      }}
+    >
+      <h1 className="text-4xl font-bold text-white">Pedro Raposo</h1>
+      <h2 className="text-2xl font-bold text-white">Frontend Developer</h2>
+    </Section>
+  );
+};
